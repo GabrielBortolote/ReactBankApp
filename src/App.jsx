7462721bb9c173fdbaa254f573bdf67905507608
BortoBoy <gbortolote@gmail.com>
2023-07-11 18:58:30 -0300

    :tada: Initial commit
    Initializing the project with standart setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './styles'
+
+const App = () => (
+  <div className='bg-primary w-full overflow-hidden'>
+    <div className={`${styles.paddingX}`}></div>
+  </div>
+);
+
+export default App
